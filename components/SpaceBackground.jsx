57bdca71c58b61a62f148ac840d5beee81e700fd
csendes-karoly <csendes.karoly@students.jedlik.eu>
2026-06-01 12:12:15 +0200

    kiemeles mindenre

--- components/SpaceBackground.jsx
+++ components/SpaceBackground.jsx
@@ -50,7 +50,7 @@ export default function SpaceBackground() {
   return (
     <canvas
       ref={canvasRef}
-      className="fixed top-0 left-0 w-full h-full -z-10"
+      className="fixed top-0 left-0 w-full h-full -z-10 "
     />
   );
 }
